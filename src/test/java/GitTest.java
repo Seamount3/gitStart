21e10a2da3b5b47143dc6a8a9687097683d1fdc4
seamount3 <yuedu25@163.com>
2023-04-15 20:09:27 +0800

    hot-fix commit1

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("少时诵诗书");
         System.out.println("少时诵诗书");
+        System.out.println("仰天大笑出门去,我辈岂是蓬蒿人!");
     }
 }
