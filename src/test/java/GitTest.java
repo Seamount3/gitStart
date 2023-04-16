9a26c02d5fb3a688546c55bff29f3f92d07db6a6
seamount3 <yuedu25@163.com>
2023-04-16 10:13:27 +0800

    Push commit2

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("仰天大笑出门去,我辈岂是蓬蒿人!");
         System.out.println("两岸猿声啼不住,轻舟已过万重山");
         System.out.println("桃花潭水深千尺,不及汪伦送我情!");
+        System.out.println("金樽清酒斗十千,玉盘珍馐值万钱");
     }
 }
